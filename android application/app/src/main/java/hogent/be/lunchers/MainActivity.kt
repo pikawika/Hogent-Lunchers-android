21ded36487b4b1e4b2d8d90a2a92739277b51d40
Bram De Coninck <Bram@bramdeconinck.com>
2018-10-18 17:38:22 +0200

    Added a basic recyclerview to the app

--- android application/app/src/main/java/hogent/be/lunchers/MainActivity.kt
+++ android application/app/src/main/java/hogent/be/lunchers/MainActivity.kt
@@ -3,6 +3,10 @@ package hogent.be.lunchers
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.ActionBar
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.LinearLayout.VERTICAL
+import hogent.be.lunchers.adapters.MyAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,7 +14,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //We maken gebruik van een custom app bar
         supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
         supportActionBar?.setCustomView(R.layout.abs_layout)
+
+        //Het inladen van de gegevens in de RecyclerView
+        initRecyclerView()
     }
+
+    private fun initRecyclerView() {
+        val recyclerview = recyclerview_main_lunches
+
+        val lunches = intArrayOf(R.drawable.eten, R.drawable.hamburger, R.drawable.kippevleugels, R.drawable.kleurstoffen, R.drawable.wavanalles)
+
+        val names = arrayOf("Eten", "Hamburger", "Kippevleugels", "Kleurstoffen", "Wavanalles")
+
+        val lManager = LinearLayoutManager(this, VERTICAL, false)
+
+        recyclerview.layoutManager = lManager
+
+        recyclerview.adapter = MyAdapter(lunches, names, this)
+    }
+
 }
---
+++ android application/app/src/main/java/hogent/be/lunchers/adapters/MyAdapter.kt
@@ -0,0 +1,25 @@
+package hogent.be.lunchers.adapters
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import hogent.be.lunchers.R
+import hogent.be.lunchers.utils.MyHolder
+
+class MyAdapter(private val place: IntArray, private val name: Array<String>, private val mContext: Context) : RecyclerView.Adapter<MyHolder>() {
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): MyHolder {
+        val v = LayoutInflater.from(p0.context).inflate(R.layout.main_page, p0, false)
+        return MyHolder(v, mContext)
+    }
+
+    override fun getItemCount(): Int {
+        return place.size
+    }
+
+    override fun onBindViewHolder(p0: MyHolder, p1: Int) {
+        p0.index(place[p1], name[p1])
+    }
+
+}
