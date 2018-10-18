5c561799a442fd70499f72a986ff64da90a12847
Bram De Coninck <Bram@bramdeconinck.com>
2018-10-18 17:20:50 +0200

    Added a custom app bar

--- android application/app/src/main/java/hogent/be/lunchers/MainActivity.kt
+++ android application/app/src/main/java/hogent/be/lunchers/MainActivity.kt
@@ -2,11 +2,15 @@ package hogent.be.lunchers
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar?.setCustomView(R.layout.abs_layout)
     }
 }
