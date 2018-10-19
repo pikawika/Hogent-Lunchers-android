98917f83d64d67f8aecaf804948372519d12e584
Bram De Coninck <Bram@bramdeconinck.com>
2018-10-19 19:43:21 +0200

    Added all models to the android app
    Also made some dummy data because there currently isn't a REST API

--- android application/app/src/main/java/hogent/be/lunchers/adapters/MyAdapter.kt
+++ android application/app/src/main/java/hogent/be/lunchers/adapters/MyAdapter.kt
@@ -5,9 +5,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import hogent.be.lunchers.R
+import hogent.be.lunchers.model.Lunch
 import hogent.be.lunchers.utils.MyHolder
 
-class MyAdapter(private val place: IntArray, private val name: Array<String>, private val mContext: Context) : RecyclerView.Adapter<MyHolder>() {
+class MyAdapter(private val lunches: List<Lunch>, private val mContext: Context) : RecyclerView.Adapter<MyHolder>() {
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): MyHolder {
         val v = LayoutInflater.from(p0.context).inflate(R.layout.list_item, p0, false)
@@ -15,11 +16,11 @@ class MyAdapter(private val place: IntArray, private val name: Array<String>, pr
     }
 
     override fun getItemCount(): Int {
-        return place.size
+        return lunches.size
     }
 
     override fun onBindViewHolder(p0: MyHolder, p1: Int) {
-        p0.index(place[p1], name[p1])
+        p0.index(lunches[p1])
     }
 
 }
--- android application/app/src/main/java/hogent/be/lunchers/utils/MyHolder.kt
+++ android application/app/src/main/java/hogent/be/lunchers/utils/MyHolder.kt
@@ -1,5 +1,6 @@
 package hogent.be.lunchers.utils
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.View
@@ -7,20 +8,28 @@ import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
 import hogent.be.lunchers.R
+import hogent.be.lunchers.model.Lunch
 
 class MyHolder (itemView: View, private val mContext: Context) : RecyclerView.ViewHolder(itemView) {
 
-    private val iview: ImageView
-    private val tview: TextView
+    private val iv_afbeelding: ImageView
+    private val tv_naam: TextView
+    private val tv_prijs: TextView
+    private val tv_beschrijving: TextView
 
     init {
-        iview = itemView.findViewById<View>(R.id.imageview_list_item_afbeelding) as ImageView
-        tview = itemView.findViewById<View>(R.id.textview_list_item_naam) as TextView
+        iv_afbeelding = itemView.findViewById<View>(R.id.imageview_list_item_afbeelding) as ImageView
+        tv_naam = itemView.findViewById<View>(R.id.textview_list_item_naam) as TextView
+        tv_prijs= itemView.findViewById<View>(R.id.textview_list_item_prijs) as TextView
+        tv_beschrijving = itemView.findViewById<View>(R.id.textview_list_item_beschrijving) as TextView
     }
 
-    fun index(item: Int, s: String) {
-        Glide.with(mContext).load(item).into(iview)
-        tview.text = s
+    @SuppressLint("SetTextI18n")
+    fun index(lunch: Lunch) {
+        Glide.with(mContext).load(lunch.afbeeldingen[0]).into(iv_afbeelding)
+        tv_naam.text = lunch.naam
+        tv_prijs.text = "€ ${lunch.prijs}"
+        tv_beschrijving.text = lunch.beschrijving
     }
 
 }
