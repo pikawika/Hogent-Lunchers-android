44bd3b595fba492c9ce17666a325f6111c73d25b
Bram De Coninck <bramdeconinck3@gmail.com>
2018-10-31 20:44:29 +0100

    Added one more model

--- android application/app/src/main/java/hogent/be/lunchers/models/Handelaar.kt
+++ android application/app/src/main/java/hogent/be/lunchers/models/Handelaar.kt
@@ -6,7 +6,7 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class Handelaar(
     val naam: String,
-    val locatie: String?,
+    val locatie: Locatie,
     val website: String,
     val lunches: List<Lunch>,
     val promotieRange: Int,
