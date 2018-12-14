892c35218701f3f042642a31221591e9804a74bf
Bram De Coninck <bramdeconinck3@gmail.com>
2018-12-14 22:17:50 +0100

    Fixed a couple of bugs

--- android application/app/src/main/java/hogent/be/lunchers/fragments/OrderListFragment.kt
+++ android application/app/src/main/java/hogent/be/lunchers/fragments/OrderListFragment.kt
@@ -24,6 +24,8 @@ class OrderListFragment : Fragment() {
 
         orderViewModel = ViewModelProviders.of(activity!!).get(OrderViewModel::class.java)
 
+        orderViewModel.resetViewModel()
+
         val reservations = orderViewModel.reservations
 
         val adapter = OrderAdapter(activity!! as MainActivity, reservations)
@@ -45,7 +47,7 @@ class OrderListFragment : Fragment() {
     override fun onResume() {
         super.onResume()
 
-        (activity as MainActivity).supportActionBar?.title = "Reservaties"
+        (activity as MainActivity).supportActionBar?.title = getString(R.string.title_fragment_orderlist)
     }
 
 }
