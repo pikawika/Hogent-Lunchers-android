e14bfcb478791a88ff10308cbe961e34e0d578d5
Lennert Bontinck <lennertbontinck@live.be>
2018-12-16 19:13:26 +0100

    repository beschrijving voorzien

--- android application/app/src/main/java/hogent/be/lunchers/repositories/OrderRepository.kt
+++ android application/app/src/main/java/hogent/be/lunchers/repositories/OrderRepository.kt
@@ -3,6 +3,9 @@ package hogent.be.lunchers.repositories
 import hogent.be.lunchers.database.OrderDao
 import hogent.be.lunchers.models.Order
 
+/**
+ * Een room repository voor bewerkingen op de lokale opgeslagen orders.
+ */
 class OrderRepository(private val orderDao: OrderDao) {
     val orders = orderDao.getAllOrders()
 
